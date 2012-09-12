fea922aa227c8b4acad1216673c36bd000c7ec34
Sebastian Janzen <sebastian.janzen@hl-services.de>
2012-09-12 15:21:37 +0200

    Centralized Date-String-Creation.

--- syslogd.js
+++ syslogd.js
@@ -11,12 +11,6 @@ var server4 = dgram.createSocket("udp4");
 var server6 = dgram.createSocket("udp6");
 
 server4.on("message", function (msg, rinfo) {
-	var now = new Date();
-	var YY = now.getFullYear();
-	var MM = (now.getMonth() + 1);
-		if (MM < 10) { MM = '0' + MM; }
-	var DD = now.getDate();
-		if (DD < 10) { DD = '0' + DD; }
 	
 	// create folder
 	if (!fs.existsSync(config.folder + "\\" + rinfo.address)) {
@@ -24,17 +18,10 @@ server4.on("message", function (msg, rinfo) {
 	}
 	
 	// write message
-	fs.appendFileSync(config.folder + "\\" + rinfo.address + "\\" + rinfo.address + "_" + YY + "_" + MM + "_" + DD + ".txt", msg + config.end);
+	fs.appendFileSync(config.folder + "\\" + rinfo.address + "\\" + rinfo.address + "_" + formatDate() + ".txt", msg + config.end);
 });
 
 server6.on("message", function (msg, rinfo) {
-	var now = new Date();
-	var YY = now.getFullYear();
-	var MM = (now.getMonth() + 1);
-		if (MM < 10) { MM = '0' + MM; }
-	var DD = now.getDate();
-		if (DD < 10) { DD = '0' + DD; }
-	
 	// replace : in IPv6 address
 	var ip = rinfo.address.replace(/:/g, ".");
 	
@@ -44,8 +31,18 @@ server6.on("message", function (msg, rinfo) {
 	}
 	
 	// write message
-	fs.appendFileSync(config.folder + "\\" + ip + "\\" + ip + "_" + YY + "_" + MM + "_" + DD + ".txt", msg + config.end);
+	fs.appendFileSync(config.folder + "\\" + ip + "\\" + ip + "_" + formatDate() + ".txt", msg + config.end);
 });
+
+var formatDate function(date) {
+  var now = date || (new Date());
+	var YY = now.getFullYear();
+	var MM = (now.getMonth() + 1);
+		if (MM < 10) { MM = '0' + MM; }
+	var DD = now.getDate();
+		if (DD < 10) { DD = '0' + DD; }
+	return YY + "_" + MM + "_" + DD;
+}
 
 server4.on("listening", function () {
   var address = server4.address();
